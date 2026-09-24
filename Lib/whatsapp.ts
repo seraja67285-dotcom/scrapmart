16e0130c868848a4e13b2013e57c825cb9ea9192
seraja67285-dotcom <seraja67285@gmail.com>
2026-09-24 10:56:44 +0530

    Add files via upload

---
+++ Lib/whatsapp.ts
@@ -0,0 +1,4 @@
+export function whatsappLink(number: string, message: string) {
+  const digits = number.replace(/\D/g, "");
+  return `https://wa.me/${digits}?text=${encodeURIComponent(message)}`;
+}
